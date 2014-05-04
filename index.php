4c885e031a2a5cc44d5e3aaad1c6f7fec20ae3ea
GhinaNurulFadhilah <ghina.nurul@student.unsil.ac.id>
2014-05-04 19:30:10 +0700

    Create index.php
    masih ada yang harus diperbaiki.

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Layanan Web</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Myriad_Pro_300.font.js" type="text/javascript"></script>
+<!--[if lt IE 7]>
+<script type="text/javascript" src="js/ie_png.js"></script>
+<script type="text/javascript">ie_png.fix('.png, #header .row-2 ul li a, #content, .list li');</script>
+<![endif]-->
+</head>
+<body id="page1">
+<div class="tail-top">
+  <div class="tail-bottom">
+    <div class="body-bg">
+      <!-- HEADER -->
+      <div id="header">
+        <div class="extra"><img src="images/header-img.jpg" alt="" /></div>
+        <div class="row-1">
+          <div class="fleft"><a href="#"><img src="images/logo.gif" alt="" /></a></div>
+          
+        </div>
+        <div class="row-2">
+          <ul>
+            <li class="m1"><a href="index.html" class="active">Home</a></li>
+            <li class="m2"><a href="about-us.html">About</a></li>
+          </ul>
+        </div>
+        <div class="row-3"><img src="images/slogan.gif" alt="" />
+        </div>
+      </div>
+      <!-- CONTENT -->
+      <div id="content">
+        <div class="inner_copy">More <a href="#">Website Templates</a> @ Templates.com!</div>
+        <div class="tail-right">
+          <div class="wrapper">
+            <div class="col-1">
+              <div class="indent">
+                <div class="indent1">
+                  <h3>Selamat Datang di Web Kami!</h3>
+                  <p>Web service menurut W3.org mendefinisikan web service sebagai “sebuah software aplikasi yang dapat teridentifikasi oleh URI dan memiliki interface yang didefiniskan, dideskripsikan, dan dimengerti oleh XML dan juga mendukung interaksi langsung dengan software aplikasi yang lain dengan menggunakan message berbasis XML melalui protokol internet”.</p>
+                  <p>Web service adalah sebuah sofware aplikasi yang tidak terpengaruh oleh platform, ia akan menyediakan method-method yang dapat diakses oleh network. Ia juga akan menggunakan XML untuk pertukaran data, khususnya pada dua entities bisnis yang berbeda.</p>
+                  <p>Dalam pengertian yang sederhana , XML Web Services dapat di definisikan sebagai aplikasi yang diakses oleh aplikasi yang lain. Mungkin orang berpendapat itu semacam web site, tetapi itu bukan demikian. Ada perbedaan – perbedaan yang membedakan dengan web site.</p>
+                  <p class="p0">This website template has several pages: <a href="#">Home</a>, <a href="#">About us</a>.</p>
+                </div>
+                <h4>Recent Articles</h4>
+                <ul class="list">
+                  <li><strong><a href="#">Selamat Datang di Web Kami</a></strong>If you are looking for perfect IT services for your business, you will find them here!</li>
+                  <li><strong><a href="#">Our services</a></strong>The complete information regarding our services you will find on the "Service" page.</li>
+                  <li><strong><a href="#">About IT Technologies Website</a></strong>Free 1028X768 Optimized Website Template from Templates.com! We hope that you will like this template and use for your websites.</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-2">
+              <ul>
+                <li><a href="http://127.0.0.1/lw/parsingpage.html">- PHP, XML PARSING</a><br/>Untuk implementasi parsingnya kami menggunakan simple xml, dengan beberapa feed xml diantaranya ada antaranews.com, vivanews.com dan live-soccer.com. Untuk melihatnya klik tombol judul di atas.</li>
+                <li><a href="https://github.com/LayWeb/PROJECT-TUBES1">- Repository Github Kami</a></li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- FOOTER -->
+      <div id="footer">
+        <div class="indent">
+          <div class="fleft">Designed by: <a href="http://www.templates.com"><img alt="" src="images/templates-logo.gif" /></a>&nbsp; Your <a href="http://russian.templates.com/product/3d-models/">3D Models</a> Marketplace</div>
+          <div class="fright">Copyright - Type in your name here</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates</a></div></body>
+</html>
